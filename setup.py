445796419ce381dcd19b2efb382c4bf7d53fcec4
Kristian <kr.arve@gmail.com>
2020-10-26 21:40:03 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="pd-backtester",
+    version="0.0.1",
+    description="Simple backtesting engine for pandas.",
+    packages=setuptools.find_packages()
+)
