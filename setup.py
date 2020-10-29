b59d058a66ea02b9626d71ba970a64958ca5a4d0
Kristian <kr.arve@gmail.com>
2020-10-29 21:03:09 +0100

    added dependencies

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setuptools.setup(
     name="pdbacktester",
     version="0.0.1",
     description="Simple backtesting engine for pandas.",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    install_requires=["pandas"]
 )
