b13a12a9674f2ec93ff686ab003731777db811a0
kristian <kr.arve@gmail.com>
2021-09-04 13:17:53 +0200

    new functions and support for column injection

--- pdbacktester/functions.py
+++ pdbacktester/functions.py
@@ -1,4 +1,4 @@
-from pdbacktester.classes import SeriesContainer
+from pdbacktester.series_container import SeriesContainer
 
 
 FUNCTION_REGISTRY = {}
@@ -40,6 +40,27 @@ def series_container(func):
     return inner
 
 
+def series_container_inject(*columns):
+    def wrapper(func):
+        def inner(df, *args):
+            modified_args = []
+            for arg in args:
+                if isinstance(arg, SeriesContainer):
+                    modified_args.append(arg.series)
+                else:
+                    modified_args.append(arg)
+
+            injected_args = []
+            for col in columns:
+                injected_args.append(df[col])
+
+            return SeriesContainer(lambda: func(*modified_args, *injected_args))
+
+        FUNCTION_REGISTRY[func.__name__] = inner
+        return inner
+    return wrapper
+
+
 @series_container
 def moving_average(column, window_length):
     column = column.rolling(window_length).mean()
@@ -48,11 +69,31 @@ def moving_average(column, window_length):
 
 @series_container
 def highest(column, windows_length):
+    # todo: remove implicit shift
     column = column.rolling(windows_length).max().shift()
     return column
 
 
 @series_container
 def lowest(column, windows_length):
+    # todo: remove implicit shift
     column = column.rolling(windows_length).min().shift()
+    return column
+
+
+@series_container
+def standard_dev(column, window_length):
+    column = column.rolling(window_length).std()
+    return column
+
+
+@series_container
+def cumulative_return(column, window_length):
+    column = column.rolling(window_length).cumprod()
+    return column
+
+
+@series_container_inject("open", "close")
+def atr(window_length, open, close):
+    column = (open - close).rolling(window_length).mean()
     return column
