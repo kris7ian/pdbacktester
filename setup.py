e45bb9aafed2c89f62d40b3c94b524dba65a92b8
kristian <kr.arve@gmail.com>
2022-04-27 23:10:52 +0200

    add long description from markdown to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
+import os
+from pathlib import Path
+
 import setuptools
+
+version = os.environ.get("GITHUB_REF_NAME")
+if version and version.startswith("v"):
+    version = version[1:]
+else:
+    version = "0.0.1.dev0"
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="pdbacktester",
-    version="0.0.1",
+    version=version,
     description="Simple backtesting engine for pandas.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=["pandas", "pandera"],
 )
